dff6cccf48702fb7cccce9eec5b21eab71f5c137
BarakpaevOlzhas <40425093+BarakpaevOlzhas@users.noreply.github.com>
2019-06-02 23:24:46 +0600

    Add files via upload

---
+++ Calculetion/Calculetion/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Calculetion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger num = 1;
+
+            for (int i = 1;i < 100001; i++)
+            {
+                num = num * i;
+            }                        
+        }
+    }
+}
---
+++ DownloadApp/DownloadApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DownloadApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClient wc = new WebClient();
+
+            string url = "https://github.com/BarakpaevOlzhas/Unity/archive/master.zip";
+            string savePath = AppDomain.CurrentDomain.BaseDirectory;
+            string nameFile = "TestFile";
+
+            wc.DownloadFile(url, savePath + nameFile);
+        }
+    }
+}
